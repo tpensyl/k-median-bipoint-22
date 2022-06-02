38e0e1c5769bca318bcee5506f30136751fe40db
tpensyl-bw <tpensyl@bandwidth.com>
2022-06-01 22:27:33 -0400

    cleaner visual mass

--- f2-stars/util/visualizeMass.m
+++ f2-stars/util/visualizeMass.m
@@ -4,16 +4,17 @@
 Clear[VisualMass,VisualMassSquare]
 Options[VisualMass] = {ImageSize -> {40,20}};
 (* n is the number of partitions of F1 *)
-VisualMassSquare[i_,j_,mass_]:=Rectangle[{i,j},{i+1-.05,j+.85*Ceiling[mass*10]/10}]
+VisualMassSquare[i_,j_,mass_]:=Rectangle[{i,j},{i+1,j+Max[mass,.2*Ceiling@mass]}]
 VisualMass[p_List,n_,OptionsPattern[]]:=Module[{massGrid,visualGrid},
     massGrid=Table[p[[i+n(j-1)]],{j,1,3},{i,1,n}][[{3,1,2}]];
 	visualGrid=Table[VisualMassSquare[i-1,j-1,massGrid[[j,i]]],{j,1,3},{i,1,n}];
 	Graphics[
 		Join[
 			{Gray, Rectangle[{0,0},{n,3}]},
-			{Black}, visualGrid[[1]],
-			{Black}, visualGrid[[2]],
-			{Black}, visualGrid[[3]]
+			{EdgeForm[{Thickness[.01],Gray}],Black},
+			visualGrid[[1]],
+			visualGrid[[2]],
+			visualGrid[[3]]
 	],PlotRange->{{0,n},{0,3}},ImageSize->OptionValue[ImageSize],AspectRatio->Full]
 ]
 
