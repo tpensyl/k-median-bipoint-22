be3802313f974baf67dc4f0213de139270302f0d
tpensyl-bw <tpensyl@bandwidth.com>
2022-06-28 17:53:14 -0400

    add lower bound stuff

---
+++ f2-stars/lower/data/exact-forms.m
@@ -0,0 +1,196 @@
+(* ::Package:: *)
+
+\!\(
+TagBox[GridBox[{
+{
+FractionBox[
+RowBox[{
+RowBox[{"b", " ", "g"}], "-", 
+RowBox[{"2", " ", 
+SuperscriptBox["b", "2"], " ", "g"}], "+", 
+RowBox[{"b", " ", 
+SuperscriptBox["g", "2"]}]}], 
+RowBox[{"1", "+", 
+RowBox[{"2", " ", "g"}], "-", 
+RowBox[{"4", " ", "b", " ", "g"}], "+", 
+SuperscriptBox["g", "2"]}]]},
+{
+FractionBox[
+RowBox[{"b", "+", 
+RowBox[{"b", " ", "g"}], "-", 
+RowBox[{"2", " ", 
+SuperscriptBox["b", "2"], " ", "g"}]}], 
+RowBox[{"1", "+", 
+RowBox[{"2", " ", "g"}], "-", 
+RowBox[{"4", " ", "b", " ", "g"}], "+", 
+SuperscriptBox["g", "2"]}]]}
+},
+DefaultBaseStyle->"Column",
+GridBoxAlignment->{"Columns" -> {{Left}}, "ColumnsIndexed" -> {}, "Rows" -> {}, "RowsIndexed" -> {}, "Items" -> {}, "ItemsIndexed" -> {}},
+GridBoxItemSize->{"Columns" -> {{Automatic}}, "ColumnsIndexed" -> {}, "Rows" -> {{Automatic}}, "RowsIndexed" -> {}, "Items" -> {}, "ItemsIndexed" -> {}}],
+"Column"]\)
+
+
+\!\(
+TagBox[GridBox[{
+{
+FractionBox[
+RowBox[{
+RowBox[{
+RowBox[{"-", "b"}], " ", "g"}], "+", 
+RowBox[{"2", " ", 
+SuperscriptBox["b", "2"], " ", "g"}], "-", 
+RowBox[{"2", " ", "b", " ", 
+SuperscriptBox["g", "2"]}]}], 
+RowBox[{
+RowBox[{"-", "2"}], "-", 
+RowBox[{"3", " ", "g"}], "+", 
+RowBox[{"6", " ", "b", " ", "g"}], "-", 
+RowBox[{"4", " ", 
+SuperscriptBox["g", "2"]}]}]]},
+{
+FractionBox[
+RowBox[{
+RowBox[{"2", " ", "b"}], "+", 
+RowBox[{"b", " ", "g"}], "-", 
+RowBox[{"2", " ", 
+SuperscriptBox["b", "2"], " ", "g"}]}], 
+RowBox[{"2", "+", 
+RowBox[{"3", " ", "g"}], "-", 
+RowBox[{"6", " ", "b", " ", "g"}], "+", 
+RowBox[{"4", " ", 
+SuperscriptBox["g", "2"]}]}]]}
+},
+DefaultBaseStyle->"Column",
+GridBoxAlignment->{"Columns" -> {{Left}}, "ColumnsIndexed" -> {}, "Rows" -> {}, "RowsIndexed" -> {}, "Items" -> {}, "ItemsIndexed" -> {}},
+GridBoxItemSize->{"Columns" -> {{Automatic}}, "ColumnsIndexed" -> {}, "Rows" -> {{Automatic}}, "RowsIndexed" -> {}, "Items" -> {}, "ItemsIndexed" -> {}}],
+"Column"]\)
+
+
+\!\(
+TagBox[GridBox[{
+{
+FractionBox[
+RowBox[{
+RowBox[{
+RowBox[{"-", "b"}], " ", "g"}], "+", 
+RowBox[{"2", " ", 
+SuperscriptBox["b", "2"], " ", "g"}], "-", 
+RowBox[{"3", " ", "b", " ", 
+SuperscriptBox["g", "2"]}]}], 
+RowBox[{
+RowBox[{"-", "3"}], "-", 
+RowBox[{"4", " ", "g"}], "+", 
+RowBox[{"8", " ", "b", " ", "g"}], "-", 
+RowBox[{"9", " ", 
+SuperscriptBox["g", "2"]}]}]]},
+{
+FractionBox[
+RowBox[{
+RowBox[{"3", " ", "b"}], "+", 
+RowBox[{"b", " ", "g"}], "-", 
+RowBox[{"2", " ", 
+SuperscriptBox["b", "2"], " ", "g"}]}], 
+RowBox[{"3", "+", 
+RowBox[{"4", " ", "g"}], "-", 
+RowBox[{"8", " ", "b", " ", "g"}], "+", 
+RowBox[{"9", " ", 
+SuperscriptBox["g", "2"]}]}]]}
+},
+DefaultBaseStyle->"Column",
+GridBoxAlignment->{"Columns" -> {{Left}}, "ColumnsIndexed" -> {}, "Rows" -> {}, "RowsIndexed" -> {}, "Items" -> {}, "ItemsIndexed" -> {}},
+GridBoxItemSize->{"Columns" -> {{Automatic}}, "ColumnsIndexed" -> {}, "Rows" -> {{Automatic}}, "RowsIndexed" -> {}, "Items" -> {}, "ItemsIndexed" -> {}}],
+"Column"]\)
+
+
+\!\(
+TagBox[GridBox[{
+{
+FractionBox[
+RowBox[{
+RowBox[{
+RowBox[{"-", "b"}], " ", "g"}], "+", 
+RowBox[{"2", " ", 
+SuperscriptBox["b", "2"], " ", "g"}], "-", 
+RowBox[{"4", " ", "b", " ", 
+SuperscriptBox["g", "2"]}]}], 
+RowBox[{
+RowBox[{"-", "4"}], "-", 
+RowBox[{"5", " ", "g"}], "+", 
+RowBox[{"10", " ", "b", " ", "g"}], "-", 
+RowBox[{"16", " ", 
+SuperscriptBox["g", "2"]}]}]]},
+{
+FractionBox[
+RowBox[{
+RowBox[{"4", " ", "b"}], "+", 
+RowBox[{"b", " ", "g"}], "-", 
+RowBox[{"2", " ", 
+SuperscriptBox["b", "2"], " ", "g"}]}], 
+RowBox[{"4", "+", 
+RowBox[{"5", " ", "g"}], "-", 
+RowBox[{"10", " ", "b", " ", "g"}], "+", 
+RowBox[{"16", " ", 
+SuperscriptBox["g", "2"]}]}]]}
+},
+DefaultBaseStyle->"Column",
+GridBoxAlignment->{"Columns" -> {{Left}}, "ColumnsIndexed" -> {}, "Rows" -> {}, "RowsIndexed" -> {}, "Items" -> {}, "ItemsIndexed" -> {}},
+GridBoxItemSize->{"Columns" -> {{Automatic}}, "ColumnsIndexed" -> {}, "Rows" -> {{Automatic}}, "RowsIndexed" -> {}, "Items" -> {}, "ItemsIndexed" -> {}}],
+"Column"]\)
+
+
+x=\!\(\*
+TagBox[GridBox[{
+{
+FractionBox[
+RowBox[{
+RowBox[{
+RowBox[{"-", "b"}], " ", "g"}], "+", 
+RowBox[{"2", " ", 
+SuperscriptBox["b", "2"], " ", "g"}], "-", 
+RowBox[{"gammaC2", " ", "b", " ", 
+SuperscriptBox["g", "2"]}]}], 
+RowBox[{
+RowBox[{"-", "gammaC2"}], "-", 
+RowBox[{
+RowBox[{"(", 
+RowBox[{"gammaC2", "+", "1"}], ")"}], " ", "g"}], "+", 
+RowBox[{"2", 
+RowBox[{"(", 
+RowBox[{"gammaC2", "+", "1"}], ")"}], " ", "b", " ", "g"}], "-", 
+RowBox[{
+RowBox[{"gammaC2", "^", "2"}], " ", 
+SuperscriptBox["g", "2"]}]}]]},
+{
+FractionBox[
+RowBox[{
+RowBox[{
+RowBox[{"-", "gammaC2"}], " ", "b"}], "-", 
+RowBox[{"b", " ", "g"}], "+", 
+RowBox[{"2", " ", 
+SuperscriptBox["b", "2"], " ", "g"}]}], 
+RowBox[{
+RowBox[{"-", "gammaC2"}], "-", 
+RowBox[{
+RowBox[{"(", 
+RowBox[{"gammaC2", "+", "1"}], ")"}], " ", "g"}], "+", 
+RowBox[{"2", 
+RowBox[{"(", 
+RowBox[{"gammaC2", "+", "1"}], ")"}], " ", "b", " ", "g"}], "-", 
+RowBox[{
+RowBox[{"gammaC2", "^", "2"}], " ", 
+SuperscriptBox["g", "2"]}]}]]}
+},
+DefaultBaseStyle->"Column",
+GridBoxAlignment->{"Columns" -> {{Left}}, "ColumnsIndexed" -> {}, "Rows" -> {}, "RowsIndexed" -> {}, "Items" -> {}, "ItemsIndexed" -> {}},
+GridBoxItemSize->{"Columns" -> {{Automatic}}, "ColumnsIndexed" -> {}, "Rows" -> {{Automatic}}, "RowsIndexed" -> {}, "Items" -> {}, "ItemsIndexed" -> {}}],
+"Column"]\)
+
+
+Table[x,{gammaC2,1,4}]
+
+
+x[[1,1]]*gammaC2+x[[1,2]]//FullSimplify
+
+
+x[[1]]
